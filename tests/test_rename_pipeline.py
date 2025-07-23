b58dd861dd5d16b84a0923f2b3fd210c6e7ef38d
Kaiano Levine <kaiano.levine@gmail.com>
2025-07-23 16:35:03 -0500

    adding tests and github workflows

---
+++ tests/test_rename_pipeline.py
@@ -0,0 +1,9 @@
+import pytest
+from your_package import rename_pipeline
+
+def test_sanitize_filename():
+    assert rename_pipeline.sanitize_filename("Song Name (Remix)!") == "Song_Name_Remix"
+
+def test_get_metadata_missing_file():
+    result = rename_pipeline.get_metadata("nonexistent_file.mp3")
+    assert result is None
